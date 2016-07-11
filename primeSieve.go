54b36bd5379a8fba7f77b4c31f2da4f2bc69d16c
sprax <sprax@comcast.net>
2016-07-11 17:18:13 -0400

    fmt

--- primeSieve.go
+++ primeSieve.go
@@ -4,6 +4,8 @@
 
 package main
 
+import "fmt"
+
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
 func Generate(ch chan<- int) {
 	for i := 2; ; i++ {
@@ -27,16 +29,16 @@ func main() {
 	ch := make(chan int) // Create a new channel.
 	go Generate(ch)      // Launch Generate goroutine.
 	k := 0
-	for i := 0; i < 200; i++ {
+	for i := 0; i < 9590; i++ {
 		prime := <-ch
-		print(prime, "  ")
+		fmt.Printf("%6d ", prime)
 		ch1 := make(chan int)
 		go Filter(ch, ch1, prime)
 		ch = ch1
 		k++
-		if k == 20 {
+		if k == 10 {
 			k = 0
-			print("\n")
+			fmt.Print("\n")
 		}
 	}
 }
