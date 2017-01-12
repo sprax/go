9c376dc7a94c832c2176940fb98ae4d6bdd1000c
Sprax Lines <sprax@talla.com>
2017-01-12 11:22:33 -0500

    types

---
+++ types.go
@@ -0,0 +1,27 @@
+
+    
+    // I found 3 ways to recognize type at runtime:
+
+    // Using string formatting
+    func typeof(v interface{}) string {
+            return fmt.Sprintf("%T", v)
+    }
+
+    // Using reflect package
+    func typeof(v interface{}) string {
+            return reflect.TypeOf(v).String()
+    }
+
+    // Using type assertions
+    func typeof(v interface{}) string {
+        switch t := v.(type) {
+            case int:
+                return "int"
+            case float64:
+                return "float64"
+            //... etc
+            default:
+                _ = t
+                return "unknown"
+        }
+    }
