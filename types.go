093c66b677e6e4f7d1f586967f79acf5bbd8cd56
Sprax Lines <sprax@talla.com>
2017-06-02 15:18:25 -0400

    switch

--- types.go
+++ types.go
@@ -1,17 +1,4 @@
-
-//  http://stackoverflow.com/questions/20170275/how-to-find-a-type-of-a-object-in-golang
-    
-    // I found 3 ways to recognize type at runtime:
-
-    // Using string formatting
-    func typeof(v interface{}) string {
-            return fmt.Sprintf("%T", v)
-    }
-
-    // Using reflect package
-    func typeof(v interface{}) string {
-            return reflect.TypeOf(v).String()
-    }
+#! /bin/env go
 
 package main
 
@@ -20,11 +7,25 @@ import (
     "reflect"
 )
 
+// Using type assertions
+func typeof(v interface{}) string {
+    switch t := v.(type) {
+        case int:
+            return "int"
+        case float64:
+            return "float64"
+        //... etc
+        default:
+            _ = t
+        return "unknown"
+    }
+}
+
 func main() {
     fmt.Println("Hello, playground")
     var (
-    N int
-    S string
+        N int
+        S string
     )
     N = 2
     S = "two"
@@ -33,21 +34,6 @@ func main() {
     
     fmt.Printf("reflect.TypeOf(N):  %s\n", reflect.TypeOf(N) )
     
-    fmt.Printf("type assertion N.(type): %v", N.(type))
-    
+    fmt.Printf("type assertion swich as func typeof: %v", typeof(N))
 }
 
-
-    // Using type assertions
-    func typeof(v interface{}) string {
-        switch t := v.(type) {
-            case int:
-                return "int"
-            case float64:
-                return "float64"
-            //... etc
-            default:
-                _ = t
-                return "unknown"
-        }
-    }
