a38920f08e234dba780da9bf6fdda09e8d414d9a
Sprax Lines <sprax@talla.com>
2017-07-15 11:37:02 -0400

    tostr

---
+++ tostr.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bool2str(val bool) string {
+	if val {
+		return "1"
+	} else {
+		return "0"
+	}
+}
+
+func int2str(val int64) string {
+	return fmt.Sprintf("%d", val)
+}
+
+func float2str(val float64) string {
+	return fmt.Sprintf("%.6f", val)
+}
+
+func main() {
+	fmt.Println("Hello, playground", float2str(3.14159), "what's up", bool2str(false))
+}
