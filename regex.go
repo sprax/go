6891265fcdce82a17d6cef43c9e2156d6acf4e7b
Sprax Lines <sprax@talla.com>
2018-01-31 01:27:06 -0500

    fix

--- regex.go
+++ regex.go
@@ -15,9 +15,9 @@ func main() {
     fmt.Println(matched, err)
 
     addTicketRE := regexp.MustCompile(`(?i)add\s+(a\s+)?ticket\s+for\s.*`)
-    matched := addTicketRE.MatchString("Add a ticket for @godzilla ")
+    matched = addTicketRE.MatchString("Add a ticket for @godzilla ")
     fmt.Println(matched)
-    matched, err := regexp.MatchString("(?i)respond\\s+to\\s.*", "Respond  to @bigfoot")
+    matched, err = regexp.MatchString("(?i)respond\\s+to\\s.*", "Respond  to @bigfoot")
     fmt.Println(matched, err)
     matched, err = regexp.MatchString("a(b", "seafood")
     fmt.Println(matched, err)
@@ -25,12 +25,12 @@ func main() {
     re := regexp.MustCompile("a(x*)b(y|z)c")
     fmt.Printf("%q\n", re.FindStringSubmatch("-axxxbyc-"))
     fmt.Printf("%q\n", re.FindStringSubmatch("-abzc-"))
-    
+
     myText := "Add a  TickeT for @bruce Are you the batman?"
     ticketOnBehalfRegex := regexp.MustCompile(`(?i)add\s+(?:a\s+)?ticket\s+for\s+(.*)`)
     matches := ticketOnBehalfRegex.FindStringSubmatch(myText)
     fmt.Printf("ticketOnBehalfRegex ~=? %s:\n\t  %#v\n", myText, matches)
-    
+
     myText = "ResPond to @alfred Where's Bruce?"
     questionOnBehalfRegex := regexp.MustCompile(`(?i)respond\s+to\s+(?-i)((<@[^|]{5,}\|[a-z0-9][a-z0-9._-]*>)|@[a-z0-9][a-z0-9._-]*)\s+(.*)`)
     matches = questionOnBehalfRegex.FindStringSubmatch(myText)
@@ -43,15 +43,14 @@ func main() {
     myText = "ResPond to <@U54321ABC|rosencrantz> Where's Guildenstern?"
     matches = questionOnBehalfRegex.FindStringSubmatch(myText)
     fmt.Printf("questionOnBehalfRegex ~=? %s:\n\t  %#v\n", myText, matches)
-    
+
     myText = "ResPond <@U54321ABC|rosencrantz> Where's Rosencrantz?"
     matches = questionOnBehalfRegex.FindStringSubmatch(myText)
     fmt.Printf("questionOnBehalfRegex ~=? %s:\n\t  %#v\n", myText, matches)
-    if (matches != nil && ) {
+    if (matches != nil) {
         fmt.Println("matches is true!  ", matches)
     } else {
         fmt.Println("matches is false!  ", matches)
     }
 }
-
 
